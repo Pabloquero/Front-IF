edaaa3cdd423d390609cec46da59252893cb53e8
Pablo Pizarro <pabloquero.misc@gmail.com>
2019-04-02 11:53:20 -0300

    Commit inicial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Inmobiliaria Fuenzalida`,
+    description: `Espacios de vida para los que buscan más - Inmobiliaria Fuenzalida`,
+    author: `Pablo Pizarro`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -10,23 +10,41 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/images/`,
       },
     },
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-sass`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Inmobiliaria Fuenzalida`,
+        short_name: `Fuenzalida`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#D8D9DD`,
+        theme_color: `#470A68`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-XXXXXXXX-X",
+        // Setting this parameter is optional (requried for some countries such as Germany)
+        anonymize: true,
+      },
+    },
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        typeName: "RootQueryWP",
+        url: "https://lychee-crisp-57351.herokuapp.com/graphql",
+        fieldName: "ifapi",
+      },
+    },
+    `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,44 @@
 import React from "react"
-import { Link } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      ifapi {
+        inicio {
+          acf {
+            tecnologia {
+              titulo
+              imagen
+              descripcion
+              alineacion
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      <h1 className="has-text-primary is-primary is-size-1">
+        {data.ifapi.inicio.acf.tecnologia.titulo}
+      </h1>
+      <p className="has-text-info is-info is-size-1">
+        Welcome to your new Gatsby site.
+      </p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
