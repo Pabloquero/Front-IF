449c51e0e1a7395415074d33ea70b817aa27bbf3
Pablo Pizarro <pabloquero.misc@gmail.com>
2019-04-10 13:23:24 -0400

    mapbox + detalles

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,19 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /mapbox-gl/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
+
 // You can delete this file if you're not using it
