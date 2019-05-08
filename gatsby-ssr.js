a07b740e94576eb6368160c081813b26b0af1355
Pablo Pizarro <pabloquero.misc@gmail.com>
2019-05-08 12:42:35 -0400

    testing ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,3 +6,21 @@
 
 // You can delete this file if you're not using it
 //export { Layout } from "./src/components/layout"
+const fetch = require("isomorphic-fetch")
+const React = require("react")
+const ApolloClient = require("apollo-boost").default
+const { ApolloProvider, getMarkupFromTree } = require("react-apollo-hooks")
+const { renderToString } = require("react-dom/server")
+
+const client = new ApolloClient({
+  uri: "https://lychee-crisp-57351.herokuapp.com/graphql",
+  fetch,
+})
+
+exports.wrapRootElement = ({ element }) => {
+  const renderedHtml = getMarkupFromTree({
+    renderFunction: renderToString,
+    tree: <ApolloProvider client={client}>{element}</ApolloProvider>,
+  })
+  return renderedHtml
+}
