0fd4e90a725240c6b2e18c562dd03bf68f5a55ee
Pablo Pizarro <pabloquero.misc@gmail.com>
2019-06-07 11:43:37 -0400

    Cambios footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,9 +11,9 @@ export default function footer() {
           </div>
         </div>
         <div className="has-text-centered has-text-white is-size-6 liFooter">
-          © {new Date().getFullYear()} Inmobiliaria Fuenzalida ·{" "}
-          <a href="https://goo.gl/maps/pWY1ENsABGr2p5KK7">
-            Alonso de Córdova 5322, piso 17, Las Condes
+          Inmobiliaria Fuenzalida ·{" "}
+          <a href="https://goo.gl/maps/18Ry5XugYjMniPsu9">
+            Alonso de Córdova 5320, piso 17, Las Condes
           </a>{" "}
           · <a href="tel:+56233231020">+56 2 3323 1020</a> ·{" "}
           <a href="mailto:info@inmobiliariafuenzalida.com">
